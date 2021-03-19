e026717eea7a2aa30c3d6bfc519d195d945a8020
nxj1129 <niko.jugovac@gmail.com>
2021-03-19 19:06:13 +0100

    Created app, added MaterialUI, done some frontend

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCIWMT_aNX9tALe7_rnxnEgEAsudOhHUXU",
+  authDomain: "reactsocialmedia-cbeed.firebaseapp.com",
+  projectId: "reactsocialmedia-cbeed",
+  storageBucket: "reactsocialmedia-cbeed.appspot.com",
+  messagingSenderId: "255157333940",
+  appId: "1:255157333940:web:54a494b18d652bcd600ce1",
+};
