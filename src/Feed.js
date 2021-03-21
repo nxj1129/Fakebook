8c7120e7a99efa75bcc628b3f9a46bd9d1942142
nxj1129 <niko.jugovac@gmail.com>
2021-03-21 19:13:53 +0100

    Feed bar added and styled, polished some UI

--- src/Feed.js
+++ src/Feed.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./Feed.css";
 import StoryReel from "./StoryReel";
 import MessageSender from "./MessageSender";
+import Post from "./Post";
 
 function Feed() {
   return (
     <div className="feed">
       <StoryReel />
       <MessageSender />
-      {/* Message Sender */}
+      <Post />
     </div>
   );
 }
--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MessageSender.css";
 import { Avatar } from "@material-ui/core";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 
 function MessageSender() {
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    //some clever db stuff
+    setInput("");
+    setImageUrl("");
   };
 
   return (
@@ -13,17 +23,37 @@ function MessageSender() {
         <Avatar />
         <form>
           <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
             className="messageSender__input"
             placeholder={`What's on your mind?`}
           />
-          <input className="" placeholder="image URL (Optional)" />
+          <input
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+            className=""
+            placeholder="image URL (Optional)"
+          />
           <button onClick={handleSubmit} type="submit">
             {" "}
             Hidden submit{" "}
           </button>
         </form>
       </div>
-      <div className="messageSender__bottom"></div>
+      <div className="messageSender__bottom">
+        <div className="messageSender__option">
+          <VideocamIcon style={{ color: "red" }} />
+          <h3>Live Video</h3>
+        </div>
+        <div className="messageSender__option">
+          <PhotoLibraryIcon style={{ color: "green" }} />
+          <h3>Photo/Video</h3>
+        </div>
+        <div className="messageSender__option">
+          <InsertEmoticonIcon style={{ color: "yellow" }} />
+          <h3>Feeling/Activity</h3>
+        </div>
+      </div>
     </div>
   );
 }
